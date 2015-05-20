4a913598cdc0f9d07a17b77a1380612c5f923447
Jeremy Petter <jeremy.petter@gmail.com>
2015-05-20 11:22:39 -0700

    initial commit

---
+++ Every.Do/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  Every.Do
+//
+//  Created by Jeremy Petter on 2015-05-20.
+//  Copyright (c) 2015 Jeremy Petter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
+
---
+++ Every.Do/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  Every.Do
+//
+//  Created by Jeremy Petter on 2015-05-20.
+//  Copyright (c) 2015 Jeremy Petter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
